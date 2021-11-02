4b2cda66354f7b7a733a1abb6a48919eb75dff5c
ilyamixaltik <mixaltik16@gmail.com>
2021-11-02 21:25:43 +0300

    create postgreSQL config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,18 @@
 import dotenv from "dotenv";
-import { Server } from '../types/config';
+import { Server, PostgreSQL } from '../types/config';
 dotenv.config({ path: __dirname + '/../../process.env' });
 
 const { SERVER_PORT } = process.env;
+const { POSTGRE_SQL_USER, POSTGRE_SQL_PASSWORD, POSTGRE_SQL_HOST, POSTGRE_SQL_PORT, POSTGRE_SQL_NAME } = process.env;
 
 export const server: Server = {
     port: Number(SERVER_PORT) || 3000
+}
+
+export const postgreSQL: PostgreSQL = {
+    user: POSTGRE_SQL_USER || 'root',
+    password: POSTGRE_SQL_PASSWORD || '',
+    host: POSTGRE_SQL_HOST || 'localhost',
+    port: Number(POSTGRE_SQL_PORT) || 5432,
+    dataBaseName: POSTGRE_SQL_NAME || 'urban_smart'
 }
--- src/types/config.ts
+++ src/types/config.ts
@@ -1,3 +1,11 @@
 export type Server = {
     port: number
 }
+
+export type PostgreSQL = {
+    user: string,
+    password: string,
+    host: string,
+    port: number,
+    dataBaseName: string
+}
